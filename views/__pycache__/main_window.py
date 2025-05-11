c88af1df11f17e923445257f53db80ef5b61d38b
Anas <anas2b@outlook.com>
2025-05-11 15:34:03 +0200

    Création d'un fichier main_window qui sera la fenêtre principale de l'application

---
+++ views/__pycache__/main_window.py
@@ -0,0 +1,19 @@
+from PySide6.QtWidgets import QMainWindow, QTabWidget
+from view.finance_view import FinanceView
+from controller.finance_controller import FinanceController
+
+class FenetrePrincipale(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Restaurant Manager")
+        self.resize(1000, 600)
+
+        self.tabs = QTabWidget()
+        self.setCentralWidget(self.tabs)
+
+        # Ajouter l'onglet Finance
+        finance_view = FinanceView()
+        finance_controller = FinanceController(finance_view)
+        finance_controller.load_and_display_data("data/reservations.json")
+
+        self.tabs.addTab(finance_view, "Finance")

0294ace55626ef0186f01885535d4d98bb9a25fe
otman <balhorotman@gmail.com>
2025-05-12 21:15:39 +0200

    debut de table view

--- views/tables_view.py
+++ views/tables_view.py
@@ -1,8 +1,19 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+import sys
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout,
+    QLabel, QPushButton, QDialog, QHBoxLayout, QFrame, QCheckBox, QScrollArea, QGroupBox, QSpinBox
+)
+from PySide6.QtCore import Qt
 
-class TablesView(QWidget):
-    def __init__(self):
-        super().__init__()
+from models.menu import CARTE_ENTREES, CARTE_PLATS, CARTE_DESSERTS, MenuItem
+
+TABLE_SIZE_MAP = {2: (120, 60), 4: (120, 80), 6: (120, 200), 8: (120, 270)}
+class TablesView(QDialog):
+    def __init__(self, table_label: str, parent=None,previous_order=None):
+        super().__init__(parent)
         layout = QVBoxLayout()
         layout.addWidget(QLabel("Vue des tables"))
         self.setLayout(layout)
+        self.occupied: bool | None = None
+        self.order: dict[str, MenuItem | None] = {"entrée": None, "plat": None, "dessert": None}
+        self.previous_order = previous_order

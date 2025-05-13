c27500c2e41f85730945f9e962d81c85c2f6055c
heussax <husseinnsakrr@gmail.com>
2025-05-13 15:09:47 +0200

    modification du fichier main.py pour afficher la fenetre principale

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from views.test import FenetrePrincipale
+from views.loginView import LoginPage
 
 if __name__ == "__main__":
+
+
     app = QApplication(sys.argv)
 
-    window = FenetrePrincipale()
-    window.show()
+    main_window = FenetrePrincipale()
+    main_window.show()
+    
+    
 
     sys.exit(app.exec())

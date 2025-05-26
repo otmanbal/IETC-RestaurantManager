6eb93420a79239bf6b10f1a145e22c174d1814c6
heussax <husseinnsakrr@gmail.com>
2025-05-26 13:07:14 +0200

    suppression de la vue Profile inutile

--- views/profileView.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-
-class ProfileView(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout(self)
-        label = QLabel("Bienvenue sur votre profil !")
-        layout.addWidget(label)

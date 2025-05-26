ac69c1aced2e16f03c61e8dd82f324f6549d5ed5
heussax <husseinnsakrr@gmail.com>
2025-05-26 12:38:54 +0200

    supression de la vue menu

--- views/menuView.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
-
-class MenuView(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("Vue des menu"))
-        self.setLayout(layout)
